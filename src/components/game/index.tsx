88e5748763c99ebd8065871c396e7b26d1236777
ig <gaibullaeviz@gmail.com>
2025-04-10 18:08:27 +0300

    feature: add time refactoring: apple position build

--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -25,7 +25,7 @@ appleAudio.volume = 0.4;
 bonusAudio.volume = 0.4;
 themeAudio.loop = true;
 
-export const Game = ({size}: IGameProps) => {
+export const Game = ({ size }: IGameProps) => {
     const isMobile = size.width <= 768;
     const hasLoaded = useRef(false);
     const started = useRef(true);
@@ -33,105 +33,113 @@ export const Game = ({size}: IGameProps) => {
     const [finished, setFinished] = useState(false);
     const canvas = useRef(null);
     const speed = useRef(0.7);
-    const play = useRef(() => {});
+    const play = useRef(() => { });
     const [score, setScore] = useState(0);
-    const apples : tApplePosition[] = [{...generateRandomPos(size, cellSize), type: "default"}];
+    const [time, setTime] = useState(0);
+    const apples: tApplePosition[] = [{ ...generateRandomPos(size, cellSize), type: "default" }];
+
+    const min = Math.floor(time / 60);
+    const sec = time % 60;
+    const formattedTime = `${min > 0 ? `${min} min` : ""} ${sec > 0 ? `${sec}` : ""} s`;
 
     // motion controller
     const processPos = {
         x: (pos: number) => {
-            if(pos >= size.width) return -size.width;
-            if(pos < -size.width) return size.width;
+            if (pos >= size.width) return -size.width;
+            if (pos < -size.width) return size.width;
             return pos
         },
         y: (pos: number) => {
-            if(pos >= size.height) return -size.height;
-            if(pos < -size.height) return size.height;
+            if (pos >= size.height) return -size.height;
+            if (pos < -size.height) return size.height;
             return pos
         },
     }
-    const direction = useRef({x: 0, y: speed.current});
+    const direction = useRef({ x: 0, y: speed.current });
     const currentDirection = useRef(direction.current);
-    const lastRotate = useRef({x: 0, y: 0});
+    const lastRotate = useRef({ x: 0, y: 0 });
     const onKeyDown = (e: KeyboardEvent) => {
         switch (e.code) {
             case "ArrowUp":
             case "KeyW":
-                if(currentDirection.current.y > 0) return
-                direction.current = {x: 0, y: -speed.current};
+                if (currentDirection.current.y > 0) return
+                direction.current = { x: 0, y: -speed.current };
                 break;
             case "ArrowDown":
             case "KeyS":
-                if(currentDirection.current.y < 0) return
-                direction.current = {x: 0, y: speed.current};
+                if (currentDirection.current.y < 0) return
+                direction.current = { x: 0, y: speed.current };
                 break;
             case "ArrowLeft":
             case "KeyA":
-                if(currentDirection.current.x > 0) return
-                direction.current = {x: -speed.current, y: 0};
+                if (currentDirection.current.x > 0) return
+                direction.current = { x: -speed.current, y: 0 };
                 break;
             case "ArrowRight":
             case "KeyD":
-                if(currentDirection.current.x < 0) return
-                direction.current = {x: speed.current, y: 0};
+                if (currentDirection.current.x < 0) return
+                direction.current = { x: speed.current, y: 0 };
                 break;
             case "Space":
-                if(!started.current) {
+                if (!started.current) {
                     play.current();
                     started.current = true;
                     setFinished(false);
                 }
                 break;
             case "KeyR":
-                if(!started.current) window.location.reload();
+                if (!started.current) window.location.reload();
                 break;
         }
     };
 
     const { handleTouchStart, handleTouchMove } = swipeController({
-        up: () => onKeyDown({code: "ArrowUp"} as KeyboardEvent),
-        down: () => onKeyDown({code: "ArrowDown"} as KeyboardEvent),
-        right: () => onKeyDown({code: "ArrowRight"} as KeyboardEvent),
-        left: () => onKeyDown({code: "ArrowLeft"} as KeyboardEvent),
+        up: () => onKeyDown({ code: "ArrowUp" } as KeyboardEvent),
+        down: () => onKeyDown({ code: "ArrowDown" } as KeyboardEvent),
+        right: () => onKeyDown({ code: "ArrowRight" } as KeyboardEvent),
+        left: () => onKeyDown({ code: "ArrowLeft" } as KeyboardEvent),
     })
 
     useEffect(() => {
-        if(hasLoaded.current){
+        if (hasLoaded.current) {
             console.log("Effect ran");
             return;
-        } 
-        if(!canvas.current || start) return
+        }
+        if (!canvas.current || start) return
         hasLoaded.current = true
         document.addEventListener("keydown", onKeyDown, false);
-        document.addEventListener("touchstart", handleTouchStart, false);        
+        document.addEventListener("touchstart", handleTouchStart, false);
         document.addEventListener("touchmove", handleTouchMove, false);
-        const space = Space(canvas.current, {scale: 1});
-        const snake =  space.addDrawable(Snake(cellSize));
-        space.addDrawable(Apple({pos: apples, size: cellSize}));
-
+        const space = Space(canvas.current, { scale: 1 });
+        const snake = space.addDrawable(Snake(cellSize));
+        space.addDrawable(Apple({ pos: apples, size: cellSize }));
+        const timer = setInterval(() => {
+            setTime((prev) => prev + 1);
+        }, 1000);
         space.draw();
         play.current = () => {
             const animation = setInterval(() => {
                 const head = snake.data.pos[0];
                 const tail = snake.data.pos[snake.data.pos.length - 1];
                 snake.data.pos.forEach((p: Position, index: number) => {
-                    if(index === 0) {
-                        p.prev = {x: p.x, y: p.y};
+                    if (index === 0) {
+                        p.prev = { x: p.x, y: p.y };
                         // process minimum range to rotate
-                        if(JSON.stringify(direction.current) !== JSON.stringify(currentDirection.current) && ((p.x + cellSize < lastRotate.current.x || p.x - cellSize > lastRotate.current.x) ||  (p.y + cellSize < lastRotate.current.y ||  p.y - cellSize > lastRotate.current.y))) {
-                            lastRotate.current = {x: p.x, y: p.y};
+                        if (JSON.stringify(direction.current) !== JSON.stringify(currentDirection.current) && ((p.x + cellSize < lastRotate.current.x || p.x - cellSize > lastRotate.current.x) || (p.y + cellSize < lastRotate.current.y || p.y - cellSize > lastRotate.current.y))) {
+                            lastRotate.current = { x: p.x, y: p.y };
                             currentDirection.current = direction.current;
                         }
                         p.x = processPos.x(p.x + currentDirection.current.x);
                         p.y = processPos.y(p.y + currentDirection.current.y);
                     } else {
-                        p.prev = {x: p.x, y: p.y};
+                        p.prev = { x: p.x, y: p.y };
                         p.x = snake.data.pos[index - 1].prev.x;
                         p.y = snake.data.pos[index - 1].prev.y;
-                        if(index > (cellSize * 3) / speed.current && checkIntersects(head, p, cellSize)) {
+                        if (index > (cellSize * 3) / speed.current && checkIntersects(head, p, cellSize)) {
                             setFinished(true);
                             started.current = false;
                             clearInterval(animation);
+                            clearInterval(timer);
                         }
                     }
                 });
@@ -150,16 +158,16 @@ export const Game = ({size}: IGameProps) => {
                             speed: 0.2,
                             audio: bonusAudio,
                         };
-                    if(checkIntersects(head, apple, eatConfig.size)) {
+                    if (checkIntersects(head, apple, eatConfig.size)) {
                         apples.splice(index, 1);
-                        for(let i = 0; i < eatConfig.bonus; i++) {
-                            snake.data.pos.push({x: tail.prev.x, y: tail.prev.y});
+                        for (let i = 0; i < eatConfig.bonus; i++) {
+                            snake.data.pos.push({ x: tail.prev.x, y: tail.prev.y });
                         }
                         eatConfig.audio.pause();
                         eatConfig.audio.currentTime = 0.5;
                         eatConfig.audio.play();
                         setScore(++snake.data.score);
-                        apples.push({...generateRandomPos(size, cellSize), type: snake.data.score % 10 === 0 ? "gold" : "default"});
+                        apples.push({ ...generateRandomPos(size, cellSize), type: snake.data.score % 10 === 0 ? "gold" : "default" });
                         speed.current = speed.current + eatConfig.speed;
                     }
                 });
@@ -168,16 +176,17 @@ export const Game = ({size}: IGameProps) => {
 
                 return () => {
                     document.removeEventListener("keydown", onKeyDown);
-                    document.removeEventListener("touchstart", handleTouchStart);        
+                    document.removeEventListener("touchstart", handleTouchStart);
                     document.removeEventListener("touchmove", handleTouchMove);
                     clearInterval(animation);
+                    clearInterval(timer);
                 }
             }, framesPerSecond);
         }
         play.current();
     }, [start]);
 
-    if(start) return <div className={styles.game}>
+    if (start) return <div className={styles.game}>
         <div className={styles.start}>
             <h1>Snake</h1>
             <p>by Islam Gaibullaev</p>
@@ -198,20 +207,26 @@ export const Game = ({size}: IGameProps) => {
     return <div className={styles.game}>
         <canvas ref={canvas} />
         <div className={styles.info}>
-            {!finished && <h3>{score}</h3>}
-            {finished && 
+            {!finished &&
+                <>
+                    <h3>{score}</h3>
+                    <h4>{formattedTime}</h4>
+                </>
+            }
+            {finished &&
                 <>
                     <h3>GAME OVER</h3>
                     <h3>{score}</h3>
-                    {!isMobile && 
+                    <h4>{formattedTime}</h4>
+                    {!isMobile &&
                         <>
                             <p>Press <b>R</b> to restart.</p>
                         </>
                     }
-                    {isMobile && 
+                    {isMobile &&
                         <div>
                             <button onClick={() => {
-                                if(!started.current) window.location.reload();
+                                if (!started.current) window.location.reload();
                             }}>Restart</button>
                         </div>
                     }
--- src/components/game/misc.ts
+++ src/components/game/misc.ts
@@ -1,7 +1,7 @@
 export type Position = {
     x: number,
     y: number,
-    prev?: {x: number, y: number}
+    prev?: { x: number, y: number }
 };
 
 export type Size = {
@@ -9,9 +9,9 @@ export type Size = {
     height: number,
 };
 
-export const generateRandomPos = (size: {width: number, height: number}, cell: number) => ({
-    x: Math.floor(Math.random() * size.width - cell),
-    y: Math.floor(Math.random() * size.height - cell),
+export const generateRandomPos = (size: { width: number, height: number }, cell: number) => ({
+    x: Math.floor(Math.random() * size.width - cell) * (Math.random() > 0.5 ? 1 : -1),
+    y: Math.floor(Math.random() * size.height - cell) * (Math.random() > 0.5 ? 1 : -1),
 });
 
 export const checkIntersects = (a: Position, b: Position, admission: number) => {
@@ -31,45 +31,45 @@ export const swipeController = ({
     right,
     left
 }: ISwipeControllerProps) => {
-    let xDown = 0;                                                        
+    let xDown = 0;
     let yDown = 0;
 
     const getTouches = (evt: TouchEvent) => {
         return evt.touches
-    }                                
-                                                                         
+    }
+
     const handleTouchStart = (evt: TouchEvent) => {
-        const firstTouch = getTouches(evt)[0];                                      
-        xDown = firstTouch.clientX;                                      
-        yDown = firstTouch.clientY;                                      
-    };                                                
-                                                                         
+        const firstTouch = getTouches(evt)[0];
+        xDown = firstTouch.clientX;
+        yDown = firstTouch.clientY;
+    };
+
     const handleTouchMove = (evt: TouchEvent) => {
-        if ( !xDown || !yDown ) {
+        if (!xDown || !yDown) {
             return;
         }
 
-        const xUp = evt.touches[0].clientX;                                    
+        const xUp = evt.touches[0].clientX;
         const yUp = evt.touches[0].clientY;
 
         const xDiff = xDown - xUp;
         const yDiff = yDown - yUp;
-                                                                            
-        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
-            if ( xDiff > 0 ) {
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) {
+            if (xDiff > 0) {
                 left();
             } else {
                 right();
-            }                       
+            }
         } else {
-            if ( yDiff > 0 ) {
+            if (yDiff > 0) {
                 up();
             } else {
                 down();
-            }                                                                 
+            }
         }
         xDown = 0;
-        yDown = 0;                                             
+        yDown = 0;
     };
     return {
         handleTouchStart,
